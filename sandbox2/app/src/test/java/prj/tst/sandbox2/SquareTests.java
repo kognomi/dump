397fd6ccaf652bb63bf4eb3ddb5785c1840beea1
kognomi <kognomi@gmail.com>
2022-11-16 18:48:42 +0300

    Create SquareTests.java

---
+++ sandbox2/app/src/test/java/prj/tst/sandbox2/SquareTests.java
@@ -0,0 +1,15 @@
+package prj.tst.sandbox2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class SquareTests {
+
+ @Test
+  public void testArea() {
+    Square s = new Square(5);
+   // assert s.area()==25;
+   Assert.assertEquals(s.area(),25);
+  }
+}
