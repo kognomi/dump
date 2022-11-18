4df545d93ffaeed743d968a5597806e57a70843d
kognomi <kognomi@gmail.com>
2022-11-18 11:21:31 +0300

    примеры сравнения строк

--- sandbox2/app/src/main/java/prj/tst/sandbox2/Equality.java
+++ sandbox2/app/src/main/java/prj/tst/sandbox2/Equality.java
@@ -3,11 +3,16 @@ package prj.tst.sandbox2;
 public class Equality {
  public static void main(String[] args) {
 
-   String s1="firefox";
-   String  s2= new String(s1);
+   String s1="firefox 2.0";
+  // String s2= "firefox "+Math.sqrt(4.0);//такое компилятор не осилит
+//String s2="firefox"; // компилятор оптимизирует код и ссылает на один и тот же объект
+   //String s2="fire"+"fox";// компилятор снова догадывается
+   //String s2=s1; .. так копируется ссылка на объект s1
+   //String  s2= new String(s1); // создается новый объект с содержимым как в s1
 
-   System.out.println(s1==s2);
-   System.out.println(s1.equals(s2));
+
+   System.out.println(s1==s2);// сравнение переменных (ссылок на объекты)
+   System.out.println(s1.equals(s2)); // сравнение содержимого переменных
  }
 
 }
