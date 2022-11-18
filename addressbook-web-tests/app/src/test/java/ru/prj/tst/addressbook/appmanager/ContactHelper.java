eeb62b69ce99744add0cc1bc7db88db3f39bff94
kognomi <kognomi@gmail.com>
2022-11-18 08:15:18 +0300

    add common methods, create helper Base

--- addressbook-web-tests/app/src/test/java/ru/prj/tst/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/app/src/test/java/ru/prj/tst/addressbook/appmanager/ContactHelper.java
@@ -12,15 +12,35 @@ public class ContactHelper {
   }
 
   public void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
     wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
     wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
     wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
     wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
     wd.findElement(By.name("home")).sendKeys(contactData.getHomePhone());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
     wd.findElement(By.name("mobile")).sendKeys(contactData.getMobilePhone());
+    wd.findElement(By.name("work")).click();
+    wd.findElement(By.name("work")).clear();
     wd.findElement(By.name("work")).sendKeys(contactData.getWorkPhone());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
     wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    wd.findElement(By.name("email2")).click();
+    wd.findElement(By.name("email2")).clear();
     wd.findElement(By.name("email2")).sendKeys(contactData.getEmail2());
+    wd.findElement(By.name("email3")).click();
+    wd.findElement(By.name("email3")).clear();
     wd.findElement(By.name("email3")).sendKeys(contactData.getEmail3());
   }
 
