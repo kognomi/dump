4f02233631433eb8ce3848898894b60527bab916
kognomi <kognomi@gmail.com>
2022-11-18 11:07:48 +0300

    так false true

--- sandbox2/app/src/main/java/prj/tst/sandbox2/Equality.java
+++ sandbox2/app/src/main/java/prj/tst/sandbox2/Equality.java
@@ -4,7 +4,7 @@ public class Equality {
  public static void main(String[] args) {
 
    String s1="firefox";
-   String  s2=s1;
+   String  s2= new String(s1);
 
    System.out.println(s1==s2);
    System.out.println(s1.equals(s2));
