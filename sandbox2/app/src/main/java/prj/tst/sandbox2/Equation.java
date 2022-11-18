1ec4288e05f1d81e2bf14f7abc613cdf7f330cb6
kognomi <kognomi@gmail.com>
2022-11-18 09:32:48 +0300

    а лучше писать вот так

--- sandbox2/app/src/main/java/prj/tst/sandbox2/Equation.java
+++ sandbox2/app/src/main/java/prj/tst/sandbox2/Equation.java
@@ -20,13 +20,12 @@ public class Equation {
     double d=b*b-4*a*c;
     if (d>0) {
      n=2;
-   } else
-      if (d == 0) {
+   } else if (d == 0) {
         n = 1;
       } else {
         n = 0;
       }
-    
+
 
 
    }
