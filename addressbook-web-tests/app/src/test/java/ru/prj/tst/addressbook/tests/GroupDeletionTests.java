bcad548cea320e581e80ca8c8bf41239f91df15e
kognomi <kognomi@gmail.com>
2022-11-19 19:23:37 +0300

    проверка на наличие группы перед удалением

--- addressbook-web-tests/app/src/test/java/ru/prj/tst/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/app/src/test/java/ru/prj/tst/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.prj.tst.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.prj.tst.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion(){
     app.getNavigationHelper().goToGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroup();
     app.getGroupHelper().returnToGroupPage();
