771199e4be10ac07d101311e914d73b8f6c90b74
kognomi <kognomi@gmail.com>
2022-11-19 20:12:51 +0300

    оптимизировали метод, сократив колво циклов

--- sandbox2/app/src/main/java/prj/tst/sandbox2/Primes.java
+++ sandbox2/app/src/main/java/prj/tst/sandbox2/Primes.java
@@ -19,4 +19,25 @@ public class Primes {
     return i==n;
 
   }
+
+
+  public static boolean isPrime(long n) {
+    for (long i=2;i<n;i++) {
+      if (n%i==0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean isPrimeFast(int n) {
+    int m = (int)Math.sqrt(n);
+    for (int i=2;i<m;i++) {
+      if (n%i==0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 }
--- sandbox2/app/src/test/java/prj/tst/sandbox2/PrimeTests.java
+++ sandbox2/app/src/test/java/prj/tst/sandbox2/PrimeTests.java
@@ -6,12 +6,23 @@ import org.testng.annotations.Test;
 public class PrimeTests {
 
   @Test
-  public void testPrimes() {
+  public void testPrime() {
     Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
   }
 
+  @Test
+  public void testPrimeFast() {
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+
+  @Test(enabled = false)
+  public void testPrimeLong() {
+    long n=Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+
     @Test
-    public void testNotPrimes() {
+    public void testNotPrime() {
       Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE-2));
     }
 
