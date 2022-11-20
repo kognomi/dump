6633d5c9818b0ef75720d1c86dbf53540130df7b
kognomi <kognomi@gmail.com>
2022-11-20 07:27:30 +0300

    в одну строчку без явного указания количества

--- sandbox2/app/src/main/java/prj/tst/sandbox2/Collections.java
+++ sandbox2/app/src/main/java/prj/tst/sandbox2/Collections.java
@@ -2,15 +2,11 @@ package prj.tst.sandbox2;
 
 public class Collections {
 
-  public static void main(String[] args){
-    String[] langs = new String[4];
-    langs[0] = "Java";
-    langs[1] = "C#";
-    langs[2] = "Python";
-    langs[3] = "PHP";
+  public static void main(String[] args) {
+    String[] langs = {"Java","C#" ,"Python", "PHP"};
   }
 
-  
+
 
 
 }
