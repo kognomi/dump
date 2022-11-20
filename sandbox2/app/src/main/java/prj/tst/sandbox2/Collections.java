cff7d0df5f7582e31e7e3832e8210b05f3dac05a
kognomi <kognomi@gmail.com>
2022-11-20 07:24:59 +0300

    создание массива, объявление каждого элемента отдельно

---
+++ sandbox2/app/src/main/java/prj/tst/sandbox2/Collections.java
@@ -0,0 +1,16 @@
+package prj.tst.sandbox2;
+
+public class Collections {
+
+  public static void main(String[] args){
+    String[] langs = new String[4];
+    langs[0] = "Java";
+    langs[1] = "C#";
+    langs[2] = "Python";
+    langs[3] = "PHP";
+  }
+
+  
+
+
+}
