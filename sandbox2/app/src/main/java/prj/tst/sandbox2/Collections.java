dedfd40a90b49109598e8fa60c9576796328843e
kognomi <kognomi@gmail.com>
2022-11-20 07:44:39 +0300

    прогнали цикл по списку

--- sandbox2/app/src/main/java/prj/tst/sandbox2/Collections.java
+++ sandbox2/app/src/main/java/prj/tst/sandbox2/Collections.java
@@ -6,15 +6,7 @@ import java.util.List;
 public class Collections {
 
   public static void main(String[] args) {
-    String[] langs = {"Java","C#" ,"Python", "PHP"};
-
-
-    List<String> languages = new ArrayList<String>();
-    languages.add("Java");
-    languages.add("C#");
-    languages.add("Python");
-    languages.add("PHP");
-
+    String[] langs = {"Java","C#" ,"Python", "PHP"};//  рамсер массива остается всегда такой как при создании, динамически его не поменять
 
     for (String l: langs) { //цикл по самими элементам массива а не по его id
       System.out.println("хочу выучить " + l);
@@ -23,6 +15,20 @@ public class Collections {
     for ( int i = 0;i<langs.length;i++) { // цикл по номерам массива
       System.out.println("хочу выучить " + langs[i]);
     }
+
+
+    List<String> languages = new ArrayList<String>();
+    languages.add("Java");
+    languages.add("C#");
+    languages.add("Python");
+    languages.add("PHP");
+
+    for (String l:languages){
+      System.out.println("хочу выучить " + l);
+    }
+
+
+
   }
 
 
