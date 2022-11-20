008178d5d3e03aeb85132d87380110ba9c923de4
kognomi <kognomi@gmail.com>
2022-11-20 10:53:00 +0300

    сравниваем множества при изменении группы

--- addressbook-web-tests/app/src/test/java/ru/prj/tst/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/app/src/test/java/ru/prj/tst/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.prj.tst.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends  TestBase{
@@ -16,12 +17,17 @@ public class GroupModificationTests extends  TestBase{
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1","bbb","ccc"));
+    GroupData group = new GroupData("test1","bbb","ccc");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size());
 
+
+    before.remove(before.size()-1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
 
   }
 }
