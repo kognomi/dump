e27a4ddbee46fa6e228bd105c5e9873ec3faa599
kognomi <kognomi@gmail.com>
2022-11-20 07:41:34 +0300

    создали список строк и наполнили его

--- sandbox2/app/src/main/java/prj/tst/sandbox2/Collections.java
+++ sandbox2/app/src/main/java/prj/tst/sandbox2/Collections.java
@@ -1,16 +1,26 @@
 package prj.tst.sandbox2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Collections {
 
   public static void main(String[] args) {
     String[] langs = {"Java","C#" ,"Python", "PHP"};
 
 
+    List<String> languages = new ArrayList<String>();
+    languages.add("Java");
+    languages.add("C#");
+    languages.add("Python");
+    languages.add("PHP");
+
+
     for (String l: langs) { //цикл по самими элементам массива а не по его id
       System.out.println("хочу выучить " + l);
     }
 
-    for ( int i = 0;i<langs.length;i++) {
+    for ( int i = 0;i<langs.length;i++) { // цикл по номерам массива
       System.out.println("хочу выучить " + langs[i]);
     }
   }
