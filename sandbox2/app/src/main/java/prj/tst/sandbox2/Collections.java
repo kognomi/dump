010caecba56f62126063031cd2e7a07fb059e11e
kognomi <kognomi@gmail.com>
2022-11-20 07:35:02 +0300

    цикл не по номерам в массиве, а по самим его элементам

--- sandbox2/app/src/main/java/prj/tst/sandbox2/Collections.java
+++ sandbox2/app/src/main/java/prj/tst/sandbox2/Collections.java
@@ -6,6 +6,10 @@ public class Collections {
     String[] langs = {"Java","C#" ,"Python", "PHP"};
 
 
+    for (String l: langs) { //цикл по самими элементам массива а не по его id
+      System.out.println("хочу выучить " + l);
+    }
+
     for ( int i = 0;i<langs.length;i++) {
       System.out.println("хочу выучить " + langs[i]);
     }
