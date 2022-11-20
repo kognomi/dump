806236d14b4626eab9381c345525ad75933f51cb
kognomi <kognomi@gmail.com>
2022-11-20 07:32:31 +0300

    стандартный for

--- sandbox2/app/src/main/java/prj/tst/sandbox2/Collections.java
+++ sandbox2/app/src/main/java/prj/tst/sandbox2/Collections.java
@@ -4,6 +4,11 @@ public class Collections {
 
   public static void main(String[] args) {
     String[] langs = {"Java","C#" ,"Python", "PHP"};
+
+
+    for ( int i = 0;i<langs.length;i++) {
+      System.out.println("хочу выучить " + langs[i]);
+    }
   }
 
 
