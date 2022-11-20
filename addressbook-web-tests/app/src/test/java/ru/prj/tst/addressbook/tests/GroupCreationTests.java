4bb8d5fbf3f9e835526712334d7ccdd63d556009
kognomi <kognomi@gmail.com>
2022-11-20 10:09:37 +0300

    добавлен метод получения списка групп со страницы групп

--- addressbook-web-tests/app/src/test/java/ru/prj/tst/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/app/src/test/java/ru/prj/tst/addressbook/tests/GroupCreationTests.java
@@ -12,11 +12,12 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().goToGroupPage();
-    List<GroupData> before
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+   // int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after,before+1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    //int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after.size(),before.size()+1);
 
   }
 
